a31f8c544dee9605c03701871b1c0ff1a218ff59
benjyp2003 <benjypfeffer@gmail.com>
2025-05-21 15:15:29 +0300

    saved classes IDF , IStrikeOption

---
+++ IDF_Operation_First_Strike/IDF.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation_First_Strike
+{
+    internal class IDF
+    {
+        public string DateOfEstablish { get; private set; } = "26/05/1948";
+        string CurrentCommander = "Eyal zamir";
+        List<IStrikeOption> strikeOptionsList = new List<IStrikeOption>();
+
+        public IDF() { }
+        public void AddToStrikeList(IStrikeOption option)
+        {
+            strikeOptionsList.Add(option);
+        }
+    }
+}
---
+++ IDF_Operation_First_Strike/StrikeOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation_First_Strike
+{
+    internal interface IStrikeOption
+    {
+        string Name { get; }
+        int RemainingStrikes { get; set; }
+        int FuelSupply { get; }
+        string TargetType { get; }
+    }
+}
