1189a3c6442b351535575c48087a53a46b625fcc
Oryan Menachem <om3160204@gmail.com>
2025-05-21 15:15:34 +0300

    1. Creation of a "Hamas" department 2. Creation of a "Terrorist" department

---
+++ IDF_Operation_First_Strike/Hamas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation_First_Strike
+{
+    internal class Hamas
+    {
+        string DateOfEstablishment { get; } = "10/12/1987";
+        string CurrentCommander { get; set; } = "Izz al-Din al-Haddad";
+        
+        //List<Terrorists> = new List<Terrorists>();
+    }
+
+
+}
---
+++ IDF_Operation_First_Strike/Terrorists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation_First_Strike
+{
+    internal class Terrorist
+    {
+        string Name { get; }
+        int Rank { get; }
+        bool IsALive { get; set; } = true;
+
+        List<string> Weapons = new List<string>();
+
+        public Terrorist(string name, int rank, List<string> weapons)
+        {
+            name = name.ToLower();
+            Rank = rank;
+            Weapons = weapons;
+        }
+
+        
+
+    }
+}
