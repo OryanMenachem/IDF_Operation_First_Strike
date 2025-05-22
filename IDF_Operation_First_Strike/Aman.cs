9460a50688beac4ee456a9def832aaa932441136
benjyp2003 <benjypfeffer@gmail.com>
2025-05-22 08:00:03 +0300

    saved class Locations

---
+++ IDF_Operation_First_Strike/Aman.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation_First_Strike
+{
+    internal class Aman
+    {
+        public Terrorist Terrorist;
+        public string LastLocation;
+        public DateTime GetTime;
+
+
+    }
+}
